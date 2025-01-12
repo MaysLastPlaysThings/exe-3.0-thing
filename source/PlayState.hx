06a29eef6236c77d8a4bfe5f92e32eab56fe914a
MaysLastPlay <109207233+MaysLastPlay@users.noreply.github.com>
2025-01-12 15:08:25 +0200

    much better

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -80,15 +80,6 @@ import Discord.DiscordClient;
 import sys.FileSystem;
 #end
 
-#if VIDEOS_ALLOWED
-#if hxCodec
-import hxcodec.VideoHandler;
-import hxcodec.VideoSprite;
-#elseif hxvlc
-//not now
-#end
-#end
-
 typedef BasicSpeedChange =
 {
 	var time:Float;
---
+++ source/import.hx
@@ -0,0 +1,10 @@
+#if !macro
+#if VIDEOS_ALLOWED
+#if hxCodec
+import hxcodec.VideoHandler;
+import hxcodec.VideoSprite;
+#elseif hxvlc
+//not now
+#end
+#end
+#end
