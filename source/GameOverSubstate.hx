42d845fb2371bee97b532d705695f8e06d5460b1
MaysLastPlay <109207233+MaysLastPlay@users.noreply.github.com>
2025-02-07 11:01:56 +0200

    update source/GameOverSubstate.hx

--- source/GameOverSubstate.hx
+++ source/GameOverSubstate.hx
@@ -84,7 +84,7 @@ class GameOverSubstate extends MusicBeatSubstate
 				bf.playAnim('firstDeath');
 			case "too-fest":
 				bf.alpha = 0;
-				var video:FlxVideo = new FlxVideo();
+				var video = new FlxVideo();
 				var file:String = Paths.video("SanicGameOvers/" + StringTools.replace(FileSystem.readDirectory(StringTools.replace(Paths.video("random"), "/random.mp4", "/SanicGameOvers"))[FlxG.random.int(0, FileSystem.readDirectory(StringTools.replace(Paths.video("random"), "/random.mp4", "/SanicGameOvers")).length)], ".mp4", ""));
 
 				trace("playing " + file);
