9dc56e038a74eb9499f08f632353ac9619489639
MaysLastPlay <109207233+MaysLastPlay@users.noreply.github.com>
2025-02-15 14:14:28 +0000

    I got lazy to continue

--- source/import.hx
+++ source/import.hx
@@ -1,5 +1,21 @@
+// Menus
 import mainmenu.MainMenuState;
+
+// Scripting
 import scripts.HScript;
 import scripts.FunkinLua;
+
+// Important Things
 import data.MP4State;
 import data.CharSongList;
+
+// Shaders
+import shaders.BlendModeEffect;
+import shaders.BlueMaskShader;
+import shaders.GreenScreenShader;
+import shaders.RetroFilter;
+import shaders.VCRDistortionShader;
+import shaders.OverlayShader;
+import shaders.StaticShader;
+import shaders.WeedVision;
+import shaders.WiggleEffect;
